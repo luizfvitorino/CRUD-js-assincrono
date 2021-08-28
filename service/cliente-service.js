68e57c0899f1e67402d52304dd90b3a22c062f53
Luiz Vitorino <luizfvitorinomoreira@gmail.com>
2021-08-28 19:42:45 -0300

    Criando função para mostrar os dados na tabela

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -1,10 +1,29 @@
+const criaCliente = (nome, email) => {
+    const linhaCliente = document.createElement('tr')
+
+    const conteudo = `
+        <td class="td" data-td>${nome}</td>
+        <td>${email}</td>
+        <td>
+            <ul class="tabela__botoes-controle">
+                <li><a href="./client-update.html" class="botao-simples botao-simples--editar">Editar</a></li>
+                <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+            </ul>
+        </td> `
+    
+    linhaCliente.innerHTML = conteudo
+    return linhaCliente
+}
+
+const tabela = document.querySelector('[data-tabela]')
+
 const http = new XMLHttpRequest()
 
 http.open('GET', 'http://localhost:3000/profile')
-
 http.send()
-
 http.onload = () => {
-    const data = http.response
-    console.log(data)
+    const data = JSON.parse(http.response)
+    data.forEach(elemento => {
+        tabela.appendChild(criaCliente(elemento.nome, elemento.email))
+    })
 }
