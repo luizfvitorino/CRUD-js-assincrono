532c5b152fd559378cff7a11d68a0683c85453b4
Luiz Vitorino <luizfvitorinomoreira@gmail.com>
2021-09-02 01:38:07 -0300

    Using async, await and IIFE in the customer editing system; fixing edit button

--- controller/listCustomers-controller.js
+++ controller/listCustomers-controller.js
@@ -1,11 +1,7 @@
 import { customerService } from "../service/customer-service.js"
 
-var rowId
-
 const createRow = (name, email, id) => {
     const newRow = document.createElement('tr') // Creates a new Table Row for the customer
-
-    rowId = id
 
     const content = `
         <td class="td" data-td>${name}</td>
@@ -31,16 +27,16 @@ table.addEventListener('click', async(event) => {
     const isDeleteButton = event.target.className === "botao-simples botao-simples--excluir"
     const isEditButton = event.target.className === "botao-simples botao-simples--editar"
 
-    if (isDeleteButton) {
-        const customerRow = event.target.closest('[data-id]')
-        const id = customerRow.dataset.id
+    const customerRow = event.target.closest('[data-id]')
+    const id = customerRow.dataset.id
 
+    if (isDeleteButton) {
         await customerService.deleteCustomer(id) // Deletes the customer from the database
         customerRow.remove() // Then deletes the customer row from the page without needing to refresh
     }
 
     if (isEditButton) {
-        window.location.href = `./customer-update.html?id=${rowId}`
+        window.location.href = `./customer-update.html?id=${id}`
     }
 })
 
@@ -48,8 +44,8 @@ const renderCustomers = async() => {
     const listCustomers = await customerService.listCustomers()
 
     listCustomers.forEach(customer => {
-        table.appendChild(createRow(customer.name, customer.email, customer.id))
-    }) // Whenever the page is refreshed, it creates a row for each customer present in the database
+            table.appendChild(createRow(customer.name, customer.email, customer.id))
+        }) // Whenever the page is refreshed, it creates a row for each customer present in the database
 }
 
 renderCustomers()
--- controller/updateCustomer-controller.js
+++ controller/updateCustomer-controller.js
@@ -1,23 +1,21 @@
 import { customerService } from "../service/customer-service.js"
 
-const catchURL = new URL(window.location) // Creates a new URL object that tells us the address of the page we are on
-const id = catchURL.searchParams.get('id') // Catches the 'id' that is in the URL
-const inputName = document.querySelector('[data-nome]')
-const inputEmail = document.querySelector('[data-email]')
+(async() => {
+    const catchURL = new URL(window.location) // Creates a new URL object that tells us the address of the page we are on
+    const id = catchURL.searchParams.get('id') // Catches the 'id' that is in the URL
+    const inputName = document.querySelector('[data-nome]')
+    const inputEmail = document.querySelector('[data-email]')
 
-customerService.detailCustomer(id) // Shows existent customer info in both inputs, on the customer update page
-    .then(customer => {
-        inputName.value = customer.name
-        inputEmail.value = customer.email
+    const data = await customerService.detailCustomer(id) // Shows existent customer info in both inputs, on the customer update page
+    inputName.value = data.name
+    inputEmail.value = data.email
+
+    const form = document.querySelector('[data-form]')
+
+    form.addEventListener('submit', async(event) => {
+        event.preventDefault()
+
+        await customerService.updateCustomer(id, inputName.value, inputEmail.value)
+        window.location.href = '../pages/update-success.html' // Once the customer is updated, it redirects to the update success page
     })
-
-const form = document.querySelector('[data-form]')
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault()
-
-    customerService.updateCustomer(id, inputName.value, inputEmail.value)
-        .then(() => {
-            window.location.href = '../pages/update-success.html' // Once the customer is updated, it redirects to the update success page
-        })
-})
+})()
