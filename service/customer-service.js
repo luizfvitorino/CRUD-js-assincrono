3d47f3cf27179b312dbdbb36198dd59f414ba8d9
Luiz Vitorino <luizfvitorinomoreira@gmail.com>
2021-09-02 02:35:47 -0300

    Using async/await in customer-service.js functions

--- service/customer-service.js
+++ service/customer-service.js
@@ -1,9 +1,12 @@
 const db = `http://localhost:3000/profile`
 
-const listCustomers = () => fetch(db).then(response => {
-        if (!response.ok) throw new Error('Não foi possível listar os clientes! Há algo de errado com a requisição.')
-        return response.json()
-    }) // Requests/receives the database info and then parses its body text into JSON
+const listCustomers = async() => {
+        const data = await fetch(db)
+
+        if (!data.ok) throw new Error('Não foi possível listar os clientes! Há algo de errado com a requisição.')
+
+        return data.json()
+    } // Requests/receives the database info, then parses its body text into JSON and returns it
 
 /* This is the code behind FETCH
 
@@ -26,47 +29,52 @@ const listCustomers = () => fetch(db).then(response => {
 
 */
 
-const createCustomer = (name, email) => {
-    return fetch(db, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                name: name,
-                email: email
-            })
+const createCustomer = async(name, email) => {
+    const data_create = await fetch(db, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
         })
-        .then(response => {
-            if (!response.ok) throw new Error('Não foi possível criar um cliente! Há algo de errado com a requisição.')
-            return response.body
-        }) // Returns Response's body
+    })
+
+    if (!data_create.ok) throw new Error('Não foi possível criar um cliente! Há algo de errado com a requisição.')
+
+    return data_create.body
 }
 
-const deleteCustomer = (id) => fetch(`http://localhost:3000/profile/${id}`, { method: 'DELETE' }).then(response => {
-    if (!response.ok) throw new Error('Não foi possível deletar o cliente! Há algo de errado com a requisição.')
-})
+const deleteCustomer = async(id) => {
+        const data_delete = fetch(`http://localhost:3000/profile/${id}`, { method: 'DELETE' })
 
-const detailCustomer = (id) => fetch(`http://localhost:3000/profile/${id}`).then(response => {
-        if (!response.ok) throw new Error('Não foi possível trazer os detalhes do cliente! Há algo de errado com a requisição.')
-        return response.json()
-    }) // Requests/receives a customer's info (using its id property) from the database and then parses into JSON
+        if (!data_delete.ok) throw new Error('Não foi possível deletar o cliente! Há algo de errado com a requisição.')
+    } // Deletes the customer from the database
 
-const updateCustomer = (id, name, email) => {
-    return fetch(`http://localhost:3000/profile/${id}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                name: name,
-                email: email
-            })
+const detailCustomer = async(id) => {
+        const data_detail = await fetch(`http://localhost:3000/profile/${id}`)
+
+        if (!data_detail.ok) throw new Error('Não foi possível trazer os detalhes do cliente! Há algo de errado com a requisição.')
+
+        return data_detail.json()
+    } // Requests/receives a customer's info (using its id property) from the database, then parses it into JSON and returns it
+
+const updateCustomer = async(id, name, email) => {
+    const data_update = await fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
         })
-        .then(response => {
-            if (!response.ok) throw new Error('Não foi possível atualizar o cliente! Há algo de errado com a requisição.')
-            return response.json()
-        })
+    })
+
+    if (!data_update.ok) throw new Error('Não foi possível atualizar o cliente! Há algo de errado com a requisição.')
+
+    return data_update.json()
 }
 
 export const customerService = {
