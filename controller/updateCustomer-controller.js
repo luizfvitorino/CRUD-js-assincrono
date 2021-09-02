2fb089e84151b8c3a5a3694b77da8aba24cf6a42
Luiz Vitorino <luizfvitorinomoreira@gmail.com>
2021-09-02 00:42:32 -0300

    Implemented customer's info update system

--- controller/updateCustomer-controller.js
+++ controller/updateCustomer-controller.js
@@ -1,7 +1,7 @@
 import { customerService } from "../service/customer-service.js"
 
-const catchURL = new URL(window.location)
-const id = catchURL.searchParams.get('id')
+const catchURL = new URL(window.location) // Creates a new URL object that tells us the address of the page we are on
+const id = catchURL.searchParams.get('id') // Catches the 'id' that is in the URL
 const inputName = document.querySelector('[data-nome]')
 const inputEmail = document.querySelector('[data-email]')
 
@@ -10,3 +10,14 @@ customerService.detailCustomer(id) // Shows existent customer info in both input
         inputName.value = customer.name
         inputEmail.value = customer.email
     })
+
+const form = document.querySelector('[data-form]')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    customerService.updateCustomer(id, inputName.value, inputEmail.value)
+        .then(() => {
+            window.location.href = '../pages/update-success.html' // Once the customer is updated, it redirects to the update success page
+        })
+})
